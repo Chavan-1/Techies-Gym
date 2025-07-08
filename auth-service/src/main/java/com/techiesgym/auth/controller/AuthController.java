9bf6724e212e6988504c8dd5809b3291cdffd707
Aarti Chavan <aartichavan090@gmail.com>
2025-07-08 09:11:04 +0530

    Added Eureka server dependencies for Service Registry

--- auth-service/src/main/java/com/techiesgym/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/techiesgym/auth/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.techiesgym.auth.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,7 @@ public class AuthController {
 		return ResponseEntity.ok(response);
 	}
 	
-	@PostMapping("/login")
+	@GetMapping("/login")
 	public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
 		AuthResponse response = authService.login(request);
 		return ResponseEntity.ok(response);
